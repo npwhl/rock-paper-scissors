76bee9f1e343eef474ecf2e2d60b27cb33921917
npwhl <taleo.sch@gmail.com>
2024-08-04 19:07:27 -0400

    Create all game functions and a call to playGame

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let selection = Math.round(Math.random()*3) + 1;
     return selection === 1 ? "rock" 
@@ -5,4 +8,50 @@ function getComputerChoice() {
         : "scissors";
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let selection = prompt("Enter your move ('Rock', 'Paper', 'Scissors').")
+    selection = selection.toUpperCase();
+    return selection === "ROCK" ? "rock"
+        : selection === "PAPER" ? "paper"
+        : selection === "SCISSORS" ? "scissors"
+        : "";
+} 
+
+function playRound(humanChoice, computerChoice) {
+    if(!humanChoice) {
+        console.log("You made an invalid move, so the computer wins by default.");
+        computerScore++;
+    }
+    if(humanChoice === computerChoice) {
+        console.log(`You drew! The computer also played ${humanChoice}`);
+        return;
+    }
+
+    let win = humanChoice === "paper" && computerChoice === "rock" ? "win" 
+        : humanChoice === "rock" && computerChoice === "scissors" ? "win"
+        : humanChoice === "scissors" && computerChoice === "paper" ? "win" 
+        : "";
+
+    if(win) {
+        console.log(`You won! ${humanChoice} beats ${computerChoice}.`) 
+        humanScore++;
+    } else {
+        console.log (`You lost! ${computerChoice} beats ${humanChoice}.`)
+        computerScore++;
+    }
+}
+
+function playGame() {
+    for(let gameCount = 0; gameCount < 5; gameCount++) {
+        playRound();
+    }
+    if(humanScore > computerScore) {
+        console.log("Winner: YOU!");
+    } else if(humanScore < computerScore) {
+        console.log("Winner: COMPUTER!");
+    } else {
+        console.log("Winner: DRAW.")
+    }
+}
+
+playGame();
